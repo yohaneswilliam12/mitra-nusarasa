e7b1f9cc367cc47b3702f227d020b38c05ba142f
William <william@is-gs.com>
2026-02-17 22:39:46 +0700

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/mitra-nusarasa',
+    assetPrefix: '/mitra-nusarasa/',
+    images: {
+        unoptimized: true,
+    },
+};
 
 module.exports = nextConfig;
