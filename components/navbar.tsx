e1befc3c055347ffd1e274f22c41246500afe85d
William <william@is-gs.com>
2026-03-19 15:37:11 +0700

    correction

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -58,7 +58,7 @@ export const Navbar = () => {
     <HeroUINavbar maxWidth="xl" position="sticky" isMenuOpen={isOpen} onMenuOpenChange={setOpen}>
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="#" onClick={(e) => {
+          <NextLink className="flex justify-start items-center gap-1" href="/" onClick={(e) => {
             e.preventDefault();
             window.scrollTo({
               top: 0,
