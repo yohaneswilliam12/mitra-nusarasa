0db935e2f6ae2f34bebe6e2bc5590da9b1912a4d
William <william@is-gs.com>
2026-03-22 14:55:55 +0700

    dark color

--- app/_components/varian.tsx
+++ app/_components/varian.tsx
@@ -4,68 +4,82 @@ import { Card, CardBody, CardHeader } from "@heroui/card";
 import Image from "next/image";
 import { Parallax } from "react-scroll-parallax";
 import { VariantReveal } from "./variant-reveal-item";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 interface ItemVariants {
     name: string
     description: string
     image: string
-    color: string
+    color?: string
+    colorDark?: string
 }
 const itemVarian: ItemVariants[] = [
     {
         name: 'Original',
         description: 'Roti lembut dengan rasa klasik yang sederhana namun tetap nikmat di setiap gigitan.',
         image: './variants/original.webp',
-        color: 'black'
     },
     {
         name: 'Blueberry',
         description: 'Perpaduan roti hangat dengan isian blueberry manis segar yang sedikit asam menyegarkan.',
         image: './variants/blueberry.webp',
-        color: 'rgb(11,11,38)'
+        color: 'rgb(11,11,38)',
+        colorDark: 'rgb(160,157,193)'
     },
     {
         name: 'Strawberry',
         description: 'Rasa strawberry manis dan harum, memberikan sensasi segar yang disukai semua kalangan.',
         image: './variants/strawberry.webp',
-        color: 'rgb(193,71,60)'
+        color: 'rgb(193,71,60)',
+        colorDark: 'rgb(232,177,171)'
     },
     {
         name: 'Matcha',
         description: 'Aroma khas matcha yang lembut dengan sentuhan pahit manis yang elegan dan menenangkan.',
         image: './variants/matcha.webp',
-        color: 'rgb(113,117,33)'
+        color: 'rgb(113,117,33)',
+        colorDark: 'rgb(245,245,205)',
     },
     {
         name: 'Cremebrulee',
         description: 'Rasa creamy dengan sentuhan karamel yang manis, menghadirkan sensasi dessert premium.',
         image: './variants/cremebrulee.webp',
-        color: 'rgb(138,48,35)'
+        color: 'rgb(138,48,35)',
+        colorDark: 'rgb(250,236,193)'
     },
     {
         name: 'Chocolate',
         description: 'Lelehan cokelat manis yang kaya rasa, cocok untuk pecinta cokelat sejati.',
         image: './variants/chocolate.webp',
-        color: 'rgb(161,71,31)'
+        color: 'rgb(161,71,31)',
+        colorDark: 'rgb(223,178,143)'
     },
     {
         name: 'Chococheese',
         description: 'Perpaduan cokelat manis dan keju gurih yang menciptakan rasa unik dan seimbang.',
         image: './variants/chococheese.webp',
-        color: 'rgb(200,151,68)'
+        color: 'rgb(200,151,68)',
+        colorDark: 'rgb(247,237,215)'
     },
     {
         name: 'Chocobanana',
         description: 'Kombinasi cokelat lembut dengan pisang manis yang menghasilkan rasa klasik favorit.',
         image: './variants/chocobanana.webp',
-        color: 'rgb(205,156,80)'
+        color: 'rgb(205,156,80)',
+        colorDark: 'rgb(252,240,215)'
     },
 ]
 
 const Item = (props: ItemVariants) => {
+    const { resolvedTheme } = useTheme();
+    const [theme, setTheme] = useState<string | undefined>(undefined);
+    useEffect(() => {
+        setTheme(resolvedTheme);
+    }, [resolvedTheme]);
     return <Card className="py-4">
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start gap-2">
-            <p className={`transition-all text-xl md:text-2xl font-bold`} style={{ color: props.color }}>{props.name}</p>
+            <p className={`transition-all text-xl md:text-2xl font-bold`} style={{ color: theme === 'dark' ? props.colorDark : props.color }}>{props.name}</p>
             <small className="text-default-500 min-h-20">{props.description}</small>
         </CardHeader>
         <CardBody className="overflow-visible py-2 h-60">
